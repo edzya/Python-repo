341920dc81fdf70643d774f442fb64a44289cb7f
edzya <edgars.zoltners@gmail.com>
2021-08-23 18:41:52 +0300

    update

--- day3.py
+++ day3.py
@@ -1,35 +1,35 @@
-# # Day 3
+# Day 3
 
-# # Task 1
+# Task 1
 
-# userTemp = float(input('Please enter your body temperature: '))
+userTemp = float(input('Please enter your body temperature: '))
 
-# if userTemp < 35:  # if users body temperature below 35 degrees
-#     print("Isn't it too cold for you ?")
-# elif userTemp <= 37:  # if users body temperature is equal or below 37 degrees
-#     print("Your body temperature is fine")
-# elif userTemp > 37:  # if users body temperature exceeds 37 degrees
-#     print("Possible fewer!")
-# else:  # error handling ? :)
-#     print("Implausible value")
+if userTemp < 35:  # if users body temperature below 35 degrees
+    print("Isn't it too cold for you ?")
+elif userTemp <= 37:  # if users body temperature is equal or below 37 degrees
+    print("Your body temperature is fine")
+elif userTemp > 37:  # if users body temperature exceeds 37 degrees
+    print("Possible fewer!")
+else:  # error handling ? :)
+    print("Implausible value")
 
 
-# # Task 2
+# Task 2
 
-# coeffcnt = 0.15
-# salary = int(input('Please enter your monthly salary: '))
-# print()
-# yrsWrkd = int(
-#     input('Please enter how much years you already work for the company: '))
-# print()
+coeffcnt = 0.15
+salary = int(input('Please enter your monthly salary: '))
+print()
+yrsWrkd = int(
+    input('Please enter how much years you already work for the company: '))
+print()
 
-# if yrsWrkd > 2:
-#     bonus = round((salary * coeffcnt) * (yrsWrkd - 2), 2)  # calculates bonus
-#     # outputs bonus to user
-#     print(f'This Christmas you will receive {bonus} EUR as a bonus!!! :)')
-# else:
-#     # if condition over 2 years was not met
-#     print('Sorry you cant receieve bonus')
+if yrsWrkd > 2:
+    bonus = round((salary * coeffcnt) * (yrsWrkd - 2), 2)  # calculates bonus
+    # outputs bonus to user
+    print(f'This Christmas you will receive {bonus} EUR as a bonus!!! :)')
+else:
+    # if condition over 2 years was not met
+    print('Sorry you cant receieve bonus')
 
 
 # Task 3
