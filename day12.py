e8d20a87b1ac03100bf78805bd12a63eab30c8f0
edzya <edgars.zoltners@gmail.com>
2021-10-01 10:14:13 +0300

    update

--- day12.py
+++ day12.py
@@ -1,12 +1,20 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-x = list(range(11))
-y = [n**2 for n in x]
-y2 = [n+5 for n in y]
-y3 = [n+15 for n in y]
+# x = list(range(11))
+# y = [n**2 for n in x]
+# y2 = [n+5 for n in y]
+# y3 = [n+15 for n in y]
 
-plt.bar(x, y)
-plt.plot(x, y2)
-plt.scatter(x, y3)
-plt.savefig("simplegrapgh.png")
-plt.show()
+# plt.bar(x, y)
+# plt.plot(x, y2)
+# plt.scatter(x, y3)
+# plt.savefig("simplegrapgh.png")
+# plt.show()
+
+import random
+from collections import Counter
+
+six_dices = [random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6) +
+             random.randint(1, 6) + random.randint(1, 6) + random.randint(1, 6) for n in range(100_000)]
+top_values = Counter(six_dices)
+print(top_values)
